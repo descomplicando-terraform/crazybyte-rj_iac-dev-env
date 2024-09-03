05d20bd0068ac152822cb8dc7f019fd96547f99b
Fabio T. Vieira <crazybyte.rj@gmail.com>
2024-09-03 10:40:39 -0300

    adicionada a descrição no output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "ip" {
-  value = libvirt_domain.servidores[*].network_interface[0].addresses[0]
+  description = "Exibe o(s) endereço(s) IP(s) atribuído(s) à(s) instância(s)"
+  value       = libvirt_domain.servidores[*].network_interface[0].addresses[0]
 }
